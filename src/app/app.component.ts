d5796e3b2e4dfca4fcd7d345f2ebae0a7e02ce9a
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2024-03-20 20:54:15 +0530

    conflict resolving

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,12 @@ import {HeroComponent} from "./hero/hero.component";
 import {HeaderComponent} from "./header/header.component";
 import {ResumeGenerateComponent} from "./resume-generate/resume-generate.component";
 import {JobFuseComponent} from "./job-fuse/job-fuse.component";
-import {SpotlightComponent} from "./spotlight/spotlight.component";
-import {FooterComponent} from "./footer/footer.component";
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, HeroComponent, HeaderComponent, ResumeGenerateComponent, JobFuseComponent, SpotlightComponent, FooterComponent],
+    imports: [RouterOutlet, HeroComponent, HeaderComponent, ResumeGenerateComponent, JobFuseComponent,FooterComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
